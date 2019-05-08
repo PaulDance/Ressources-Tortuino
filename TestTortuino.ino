d8a14819b89c72c4f88e3d8db46f52b5f977ca55
Al3xCalibur <montesquieu.amis@hotmail.fr>
2019-05-08 23:59:32 +0200

    Ajout de tous les premiers fichiers modèles de Fusion 360, la découpeuse laser et l'impression 3D

--- TestTortuino.ino
+++ TestTortuino.ino
@@ -7,16 +7,42 @@
 
 
 void setup() {
-  initialiser();
-  vitesse(10);
+  int attente = 500;
   
-  avancer(5);
-  reculer(5);
-  tournerGauche(30);
-  tournerDroite(30);
+  initialiser("v");
+  vitesse(10);
 
-  delay(1000);
-  arbre(3, 5);
+  monterFeutre();// Test du feutre tout seul.
+  descendreFeutre();
+  delay(attente);
+  
+  avancer(5);         // Test des roues : avant-arrière.
+  reculer(5);
+  delay(attente);
+  
+  tournerGauche(60);  // Test des roues : gauche-droite.
+  tournerDroite(60);
+  delay(attente);//même avec montée et descente du feutre
+  
+  descendreFeutre();  // Test pointillés.
+  avancer(2);
+  monterFeutre();
+  avancer(2);
+  descendreFeutre();
+  avancer(2);
+  delay(attente);
+
+  monterFeutre();     // Vérification de la continuité d'un trait,
+  avancer(1);         // même avec montée et descente du feutre.
+  descendreFeutre();
+  avancer(2);
+  monterFeutre();
+  descendreFeutre();
+  avancer(2);
+  delay(attente);
+  
+  carre(10);        // Test complet : un arbre à 3 niveaux.
+  
 }
 
 void loop() {
